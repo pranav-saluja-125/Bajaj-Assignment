98ceb600109acb17fd0e19301e2dfe77ad8f0c40
Pranav Saluja <111756527+pranav-saluja-125@users.noreply.github.com>
2024-03-18 10:54:45 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,69 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const app = express();
+
+
+app.use(bodyParser.json());
+
+
+app.get('/',(req,res)=>{
+    res.write("Hello World");
+    res.end();
+})
+
+let response={
+
+};
+
+app.get('/bfhl',(req,res)=>{
+    let list=JSON.stringify(response);
+    res.write(list);
+    res.end();
+})
+
+app.post('/bfhl', (req, res) => {
+
+        const data = req.body.data;
+
+
+        const user_id = "john_doe_17091999";
+
+
+        const email = "john@xyz.com";
+        const roll_number = "ABCD123";
+        const odd_numbers = [];
+        const even_numbers = [];
+        const alphabets = [];
+
+
+        data.forEach(element => {
+            if (typeof element === 'string' && isNaN(element)) {
+                alphabets.push(element.toUpperCase());
+            } else if (!isNaN(element)) {
+                if (parseInt(element) % 2 === 0) {
+                    even_numbers.push(element);
+                } else {
+                    odd_numbers.push(element);
+                }
+            }
+        });
+
+
+        response = {
+            is_success: true,
+            user_id: user_id,
+            email: email,
+            roll_number: roll_number,
+            odd_numbers: odd_numbers,
+            even_numbers: even_numbers,
+            alphabets: alphabets
+        };
+        res.json(response);
+});
+
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
